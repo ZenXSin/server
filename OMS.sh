9ecee11e5e91a38f65954e4573904695d58c071b
ZenXSin <119043044+ZenXSin@users.noreply.github.com>
2023-05-18 07:13:52 +0800

    Add files via upload

--- OMS.sh
+++ OMS.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-# 安装sudo、wget和unzip命令
-apt-get update
-apt-get install -y sudo wget unzip
+# 内置版本号
+version="1.0.1"
+
+echo "正在下载最新版本..."
+wget -q https://raw.githubusercontent.com/ZenXSin/server/main/OMS.sh -O OMS.sh
+echo "下载完成，当前版本$version"
 
 echo "你好，请确认你目前已经使用screen创建了一个新的会话，并且当前目录是你要开服的服务器目录。"
 select option in "是的，继续安装" "我没有screen，选我安装" "我不是在我的服务器目录，点我退出" "其他原因，退出"
@@ -26,9 +29,9 @@ echo "无效选项"
 ;;
 esac
 done
-read -p "是否安装Java8或以上版本？(是/否) " java_option
-if [ "$java_option" = "是" ]; then
-echo "开始安装Java8或以上版本"
+read -p "是否安装Java8或以上版本？(y/n) " java_option
+if [ "$java_option" = "y" ]; then
+echo "开始安装Java8"
 sudo apt-get install -y openjdk-8-jdk || { echo -e "\033[31m安装Java失败\033[0m"; exit 1; }
 fi
 echo "正在下载server.zip"
